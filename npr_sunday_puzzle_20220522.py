2705d6dffec49e4b5ad86e4c6ed41794b37aa8a8
isadore <isadore@tuta.io>
2022-05-22 10:49:05 -0700

    Function to answer 2022-05-22 NPR Sunday Puzzle

---
+++ npr_sunday_puzzle_20220522.py
@@ -0,0 +1,66 @@
+"""
+NPR Sunday Puzzle
+2022-05-22
+
+Take the name of an island. Move its first letter two spaces later in
+the alphabet (so A would become C, B would become D, etc.). Reverse
+the result and you'll have the name of another island. What islands
+are these?
+
+https://www.npr.org/2022/05/22/1100539535/sunday-puzzle-just-a-d-away
+
+"""
+
+
+def island_puzzle(island: str) -> str:
+    """Returns island with first letter shifted two spaces later in the
+    alphabet, then reversed.
+    """
+
+    abc = "abcdefghijklmnopqrstuvwxyz"
+    abc_shift = abc[2:] + abc[0:2]
+
+    abc_key = {k: v for k, v in list(zip(abc, abc_shift))}
+
+    island_shift = [
+        abc_key[letter] if count == 0 else letter
+        for count, letter in enumerate(island.lower())
+    ]
+    island_str = "".join(island_shift)
+
+    result = island_str[::-1]
+
+    return result
+
+
+islands = [
+    "Aaaab",
+    "Baaac",
+    "Aviation Island",
+    "Bird Island",
+    "Bunker Island",
+    "Cooper-Meng Island",
+    "Eastern Island",
+    "Engineer Island",
+    "Holei Island",
+    "Home Islets",
+    "Kaula Island",
+    "Marine Island",
+    "Papala Island",
+    "Paradise Island",
+    "Pelican Island",
+    "Quail Island",
+    "Sand Island",
+    "Strawn Island",
+    "Tanager Island",
+    "Whippoorwill Island",
+]
+
+islands = [island.lower() for island in islands]
+
+answer = []
+for island in islands:
+    result = island_puzzle(island)
+    if result in islands:
+        answer.append((island, result))
+        print((island, result))
